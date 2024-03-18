e4e9fdce37157c5d9561761a4e4753e8fae91cbb
pariakhodaveissi <paria.khodaveissi@gmail.com>
2024-03-18 10:28:19 +0000

    change the get.app for the path '/api/:date?' completely to a new approach, since the previous code was not correct due to tests

--- index.js
+++ index.js
@@ -19,35 +19,30 @@ app.get("/", function (req, res) {
 });
 
 // API endpoint to process date input
-app.get('/api/:date?', (req, res)=>{
-  let dateString = req.params.date;
-  console.log('Received date:', dateString); // Debug log
-  let date;
-
-  if (!dateString) {
-    date = new Date();
-  } else {
-    // Explicitly parse as integer for Unix timestamp
-    const timestamp = parseInt(dateString);
-    if (!isNaN(timestamp)) {
-      // Create a date object using the timestamp if it's a valid number
-      date = new Date(timestamp);
-    } else {
-      // Otherwise, try to create a date object directly from the input string
-      date = new Date(dateString);
-    }
+app.get('/api/:date?', function(req, res){
+  let date = req.params.date;
+  let unixDate;
+  let dateObj;
+  //if is number
+  let isUnix = /^\d+$/.test(date);
+  if(!date){
+    dateObj = new Date();
   }
-
-  console.log('Date object:', date); // Debug log
-
-  if (date.toString() === "Invalid Date") {
-    console.log('Invalid Date');
+  else if(date && isUnix){
+    unixDate = parseInt(date);
+    dateObj = new Date(unixDate);
+  }
+  else if(date && !isUnix) {
+    dateObj = new Date(date);
+    
+  }
+  if(dateObj.toString() === "Invalid Date"){
     res.json({error: "Invalid Date"});
-  } else {
-    const response = {unix: date.getTime(), utc: date.toUTCString()};
-    console.log('Response:', response); // Debug log
-    res.json(response);
+    return;
   }
+  unixDate = dateObj.getTime();
+  utcDate = dateObj.toUTCString();
+  res.json({unix: unixDate, utc: utcDate});
 });
 
 // Listen on port set in environment variable or default to 3000
